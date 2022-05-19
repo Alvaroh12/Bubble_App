d03fdca8760d3fcaeb52e5384b658b10496383cf
Alvaroh12 <6002128@viewnext.com>
2022-05-19 09:29:52 +0200

    Initial commit

---
+++ app/src/main/java/com/alvaroh12/bubble/Model/Empleo.java
@@ -0,0 +1,74 @@
+package com.alvaroh12.bubble.Model;
+
+public class Empleo {
+
+    private int id_empleo;
+    private int isAceptado;
+    private int isCancelado;
+    private double id_oferta;
+    private int id_usuario_comprador;
+    private int id_usuario_oferente;
+
+    public void Empleo(){
+
+    }
+
+    public Empleo(int id_empleo, int isAceptado, int isCancelado, double id_oferta, int id_usuario_comprador,
+                  int id_usuario_oferente) {
+        this.id_empleo = id_empleo;
+        this.isAceptado = isAceptado;
+        this.isCancelado = isCancelado;
+        this.id_oferta = id_oferta;
+        this.id_usuario_comprador = id_usuario_comprador;
+        this.id_usuario_oferente = id_usuario_oferente;
+    }
+
+    public int getId_empleo() {
+        return id_empleo;
+    }
+
+    public void setId_empleo(int id_empleo) {
+        this.id_empleo = id_empleo;
+    }
+
+    public int getIsAceptado() {
+        return isAceptado;
+    }
+
+    public void setIsAceptado(int isAceptado) {
+        this.isAceptado = isAceptado;
+    }
+
+    public int getIsCancelado() {
+        return isCancelado;
+    }
+
+    public void setIsCancelado(int isCancelado) {
+        this.isCancelado = isCancelado;
+    }
+
+    public double getId_oferta() {
+        return id_oferta;
+    }
+
+    public void setId_oferta(double id_oferta) {
+        this.id_oferta = id_oferta;
+    }
+
+    public int getId_usuario_comprador() {
+        return id_usuario_comprador;
+    }
+
+    public void setId_usuario_comprador(int id_usuario_comprador) {
+        this.id_usuario_comprador = id_usuario_comprador;
+    }
+
+    public int getId_usuario_oferente() {
+        return id_usuario_oferente;
+    }
+
+    public void setId_usuario_oferente(int id_usuario_oferente) {
+        this.id_usuario_oferente = id_usuario_oferente;
+    }
+
+}
---
+++ app/src/main/java/com/alvaroh12/bubble/Model/Oferta.java
@@ -0,0 +1,63 @@
+package com.alvaroh12.bubble.Model;
+
+public class Oferta {
+
+    private int id_oferta;
+    private String tipo_oferta;
+    private String descripcion;
+    private double precio;
+    private int id_usuario;
+
+    public void Oferta(){
+
+    }
+
+    public Oferta(int id_oferta, String tipo_oferta, String descripcion, double precio, int id_usuario) {
+        this.id_oferta = id_oferta;
+        this.tipo_oferta = tipo_oferta;
+        this.descripcion = descripcion;
+        this.precio = precio;
+        this.id_usuario = id_usuario;
+    }
+
+    public int getId_oferta() {
+        return id_oferta;
+    }
+
+    public void setId_oferta(int id_oferta) {
+        this.id_oferta = id_oferta;
+    }
+
+    public String getTipo_oferta() {
+        return tipo_oferta;
+    }
+
+    public void setTipo_oferta(String tipo_oferta) {
+        this.tipo_oferta = tipo_oferta;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public int getId_usuario() {
+        return id_usuario;
+    }
+
+    public void setId_usuario(int id_usuario) {
+        this.id_usuario = id_usuario;
+    }
+
+}
